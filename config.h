97f4bec05436a26e15d7070fa038884670b777ba
alefir <alefir@cock.li>
2018-04-18 18:10:23 -0700

    did some stuff

--- config.h
+++ config.h
@@ -34,7 +34,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -61,6 +61,7 @@ static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[] = { "surf", "-g", "-d", "https://duckduckgo.com", NULL };
 static const char *lockcmd[] = { "slock", NULL };
 static const char *irccmd[] = { "st", "weechat", NULL };
+static const char *nmcmd[] = { "networkmanager_dmenu", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -68,6 +69,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_n,      spawn,          {.v = nmcmd } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = -1 } },
