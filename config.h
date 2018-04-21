35bcf5fb1b34b7185771f0fbc7fa4116ddf80975
Alefir Ulfhiemon <alefir@cock.li>
2018-04-21 16:28:20 -0700

    Change browser to Firefox from Surf

--- config.h
+++ config.h
@@ -28,7 +28,6 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -58,7 +57,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *browsercmd[] = { "surf", "-g", "-d", "https://duckduckgo.com", NULL };
+static const char *browsercmd[] = { "firefox", NULL };
 static const char *lockcmd[] = { "slock", NULL };
 static const char *irccmd[] = { "st", "weechat", NULL };
 static const char *nmcmd[] = { "networkmanager_dmenu", NULL };
